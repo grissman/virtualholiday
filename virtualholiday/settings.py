901f9811b8f296ec49c6454d8bffa5c5f8448ea1
Gabe Rissman <grissman@gmail.com>
2020-04-10 16:39:31 -0400

    can id eploy

--- virtualholiday/settings.py
+++ virtualholiday/settings.py
@@ -73,12 +73,11 @@ WSGI_APPLICATION = 'virtualholiday.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
 
 
 
